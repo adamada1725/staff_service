4f66ef58c7e99a6f8748a9753ffba10af0780cd8
adamada1725 <adamada1725@gmail.com>
2024-11-03 04:05:06 +0300

    feat(api): create universal CRUD

--- api/src/employee/crud.py
+++ api/src/employee/crud.py
@@ -1,27 +1,42 @@
-from typing import List, Optional
+from typing import List, Optional, TypeVar, Type
 
-from sqlalchemy import select
+from pydantic import BaseModel
+from sqlalchemy import select, update, delete
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Response, status
 
 from src.employee.schemas import CreateEmployee
 from src.models.employee import Employee
+from src.database import Base
 
-async def get_employees(limit: int, session: AsyncSession) -> List[Employee]:
-    stmt = select(Employee).order_by(Employee.id).limit(limit)
-    result: Result = await session.execute(stmt)
-    employees = result.scalars().all()
-    return employees
+M = TypeVar("M", bound=Base)
+S = TypeVar("S", bound=BaseModel)
 
-async def get_employee_by_id(session: AsyncSession, employee_id: int) -> Optional[Employee]:
-    stmt = select(Employee).where(Employee.id == employee_id)
-    result: Result = await session.execute(stmt)
-    employee = result.scalar_one_or_none()
-    return employee
+async def get_models(model: Type[M], limit: int, session: AsyncSession) -> List[M]:
+    stmt = select(model).limit(limit)
+    result = await session.execute(stmt)
+    return result.scalars().all()
 
-async def create_employee(session: AsyncSession, data: CreateEmployee) -> Employee:
-    employee = Employee(**data.model_dump())
-    session.add(employee)
+async def get_model_by_id(model: Type[M], id: int, session: AsyncSession) -> M:
+    result = await session.get(model, id)
+    return result
+
+async def create_model(model: Type[M], data: Type[S], session: AsyncSession) -> M:
+    parsed_model = model(**data.model_dump())
+    session.add(parsed_model)
     await session.commit()
-    await session.refresh(employee)
-    return employee
+    await session.refresh(parsed_model)
+    return parsed_model
+
+async def update_model(model: Type[M], id: int, data: Type[S], session: AsyncSession) -> None:
+    stmt = update(model).where(model.id == id).values(**data.model_dump())
+    result = await session.execute(stmt)
+    await session.commit()
+    return
+
+async def delete_model(model: Type[M], id: int, session: AsyncSession) -> None:
+    stmt = delete(model).where(model.id == id)
+    result = await session.execute(stmt)
+    await session.commit()
+    return
