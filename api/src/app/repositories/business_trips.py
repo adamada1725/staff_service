a86d4a91cb96ad2b0f48f8cd2ec8015e81a3b2f7
adamada1725 <adamada1725@gmail.com>
2024-12-21 16:30:11 +0300

    fix: remove needless class declaration

--- api/src/app/repositories/business_trips.py
+++ api/src/app/repositories/business_trips.py
@@ -1,7 +1,4 @@
 from app.models import BusinessTrip
 from base.repository import BaseTripRepository
 
-class BusinessTripsRepository(BaseTripRepository):
-    pass
-
-business_trips_repository = BusinessTripsRepository(BusinessTrip)
+business_trips_repository = BaseTripRepository(BusinessTrip)
